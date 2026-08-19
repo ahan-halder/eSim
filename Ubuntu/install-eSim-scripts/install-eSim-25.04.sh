318359079929c4eaf3ff0c1558b08a2217ae2ada
ahan-halder <halderahan.2005@gmail.com>
2026-08-19 17:31:12 +0530

    fix: correct KiCad symbol library copy command

--- Ubuntu/install-eSim-scripts/install-eSim-25.04.sh
+++ Ubuntu/install-eSim-scripts/install-eSim-25.04.sh
@@ -259,7 +259,8 @@ function copyKicadLibrary
         "$kicad_config_dir/"
 
     echo "Copying eSim custom table..."
-    sudo cp -r kicadLibrary/eSim-symbols/* \ /usr/share/kicad/symbols/
+    sudo cp -r kicadLibrary/eSim-symbols/* \
+        /usr/share/kicad/symbols/
 
     rm -rf kicadLibrary
 
